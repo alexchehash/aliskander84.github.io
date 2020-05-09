4db5e52b9bd5f41916ad575095d74b96cff40750
alexchegash <aliskander84@gmail.com>
2020-05-09 05:53:09 +0300

    rebuilt poker

---
+++ poker/precache-manifest.6ffa8ab4416c71092099640cfa3325c5.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "159a225f420e1ecbb777def4e54eba14",
+    "url": "/poker/index.html"
+  },
+  {
+    "revision": "e3d109bd01134c9adba2",
+    "url": "/poker/static/css/main.3117913b.chunk.css"
+  },
+  {
+    "revision": "4be8b964f5e7bdf1f519",
+    "url": "/poker/static/js/2.5f808321.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/poker/static/js/2.5f808321.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e3d109bd01134c9adba2",
+    "url": "/poker/static/js/main.6737cd91.chunk.js"
+  },
+  {
+    "revision": "d82b3e42e71fcee9fa43",
+    "url": "/poker/static/js/runtime-main.1b373b8b.js"
+  }
+]);
--- poker/service-worker.js
+++ poker/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.0b3495ca3c78c44dff18d9b277085686.js"
+  "/poker/precache-manifest.6ffa8ab4416c71092099640cfa3325c5.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/poker/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
