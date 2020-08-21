11337440104073ae80182ea2472b8a2c64e98686
alexchegash <aliskander84@gmail.com>
2020-08-21 17:00:21 +0300

    magifta

---
+++ magifta/precache-manifest.73782cbd57582fe9f44b73f6204b954c.js
@@ -0,0 +1,86 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e61b72c226e6fb0b1278fe55b6234293",
+    "url": "/magifta/index.html"
+  },
+  {
+    "revision": "8f1af4faf04ddfda86c5",
+    "url": "/magifta/static/css/main.fcfd5355.chunk.css"
+  },
+  {
+    "revision": "ee9890e6be55806d8a97",
+    "url": "/magifta/static/js/0.e6051e89.chunk.js"
+  },
+  {
+    "revision": "2eb6e0f044e2f82d8748",
+    "url": "/magifta/static/js/3.60904212.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/magifta/static/js/3.60904212.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "aa515614942fea4dbffa",
+    "url": "/magifta/static/js/4.e880d5d5.chunk.js"
+  },
+  {
+    "revision": "8d64cb0513b947977370",
+    "url": "/magifta/static/js/5.67f172dd.chunk.js"
+  },
+  {
+    "revision": "f900ef3774e5017f4a10",
+    "url": "/magifta/static/js/6.8100aa46.chunk.js"
+  },
+  {
+    "revision": "c2eba83bc0f21fda4353",
+    "url": "/magifta/static/js/7.ff05b385.chunk.js"
+  },
+  {
+    "revision": "af4c3e515e97969952c9",
+    "url": "/magifta/static/js/8.e1dd7a4e.chunk.js"
+  },
+  {
+    "revision": "7f14988eefee40b34eb6",
+    "url": "/magifta/static/js/9.c844c9b8.chunk.js"
+  },
+  {
+    "revision": "8f1af4faf04ddfda86c5",
+    "url": "/magifta/static/js/main.261dafde.chunk.js"
+  },
+  {
+    "revision": "4183b9f3079843f44afe",
+    "url": "/magifta/static/js/runtime-main.5cc102b2.js"
+  },
+  {
+    "revision": "a1ceb2fea0c97b38d2bc576531644da4",
+    "url": "/magifta/static/media/Manrope-ExtraBold.a1ceb2fe.woff"
+  },
+  {
+    "revision": "b13b33fce19721ed97dd2e0945aac7ed",
+    "url": "/magifta/static/media/Manrope-ExtraBold.b13b33fc.woff2"
+  },
+  {
+    "revision": "5f049513e595a19e7f8bb367cd6bdd79",
+    "url": "/magifta/static/media/Manrope-ExtraLight.5f049513.woff2"
+  },
+  {
+    "revision": "88e6544dc19a5b6b17d474770ff58b8e",
+    "url": "/magifta/static/media/Manrope-ExtraLight.88e6544d.woff"
+  },
+  {
+    "revision": "1ec84d49b840b2dce7020e886dc06fc0",
+    "url": "/magifta/static/media/Manrope-Regular.1ec84d49.woff2"
+  },
+  {
+    "revision": "86face4743712e0ca8d0629d1e28ce59",
+    "url": "/magifta/static/media/Manrope-Regular.86face47.woff"
+  },
+  {
+    "revision": "70b7b997148d8df47a0da39617dce356",
+    "url": "/magifta/static/media/Manrope-SemiBold.70b7b997.woff2"
+  },
+  {
+    "revision": "e1b25f5b8bb418e9ff79de3c15e3583c",
+    "url": "/magifta/static/media/Manrope-SemiBold.e1b25f5b.woff"
+  }
+]);
