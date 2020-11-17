8095626ea05eace239a33a0ef2d0701185d2da45
alexchegash <aliskander84@gmail.com>
2020-11-17 14:50:55 +0200

    [ADD] - magifta1

---
+++ magifta1/precache-manifest.73d67ca7f6a3bbf9aca9401999a37f59.js
@@ -0,0 +1,78 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4271e9e0eeec5c2d90229ff3fd13de79",
+    "url": "/magifta1/index.html"
+  },
+  {
+    "revision": "2f7ba2798af34eb41972",
+    "url": "/magifta1/static/css/2.76cf7f4f.chunk.css"
+  },
+  {
+    "revision": "7c64e7fd51fc49b7b216",
+    "url": "/magifta1/static/css/main.3f6feb31.chunk.css"
+  },
+  {
+    "revision": "2f7ba2798af34eb41972",
+    "url": "/magifta1/static/js/2.eaaf6318.chunk.js"
+  },
+  {
+    "revision": "450d193148fd21ba5e6e4baf2c6cc67d",
+    "url": "/magifta1/static/js/2.eaaf6318.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7c64e7fd51fc49b7b216",
+    "url": "/magifta1/static/js/main.dd9efdbb.chunk.js"
+  },
+  {
+    "revision": "619a1d2e30ad9cf0ed82",
+    "url": "/magifta1/static/js/runtime-main.ae0981c8.js"
+  },
+  {
+    "revision": "a1ceb2fea0c97b38d2bc576531644da4",
+    "url": "/magifta1/static/media/Manrope-ExtraBold.a1ceb2fe.woff"
+  },
+  {
+    "revision": "b13b33fce19721ed97dd2e0945aac7ed",
+    "url": "/magifta1/static/media/Manrope-ExtraBold.b13b33fc.woff2"
+  },
+  {
+    "revision": "5f049513e595a19e7f8bb367cd6bdd79",
+    "url": "/magifta1/static/media/Manrope-ExtraLight.5f049513.woff2"
+  },
+  {
+    "revision": "88e6544dc19a5b6b17d474770ff58b8e",
+    "url": "/magifta1/static/media/Manrope-ExtraLight.88e6544d.woff"
+  },
+  {
+    "revision": "1ec84d49b840b2dce7020e886dc06fc0",
+    "url": "/magifta1/static/media/Manrope-Regular.1ec84d49.woff2"
+  },
+  {
+    "revision": "86face4743712e0ca8d0629d1e28ce59",
+    "url": "/magifta1/static/media/Manrope-Regular.86face47.woff"
+  },
+  {
+    "revision": "70b7b997148d8df47a0da39617dce356",
+    "url": "/magifta1/static/media/Manrope-SemiBold.70b7b997.woff2"
+  },
+  {
+    "revision": "e1b25f5b8bb418e9ff79de3c15e3583c",
+    "url": "/magifta1/static/media/Manrope-SemiBold.e1b25f5b.woff"
+  },
+  {
+    "revision": "4bcc3270dc84dbe1bc438ae500c8b8c6",
+    "url": "/magifta1/static/media/img1.4bcc3270.svg"
+  },
+  {
+    "revision": "adf8705d5b6172230db3bca7355870af",
+    "url": "/magifta1/static/media/img2.adf8705d.svg"
+  },
+  {
+    "revision": "632ee12db422609e078184c4f25b7b9b",
+    "url": "/magifta1/static/media/img3.632ee12d.svg"
+  },
+  {
+    "revision": "c0e9525c985ee187c0492cc4dbea6134",
+    "url": "/magifta1/static/media/img4.c0e9525c.svg"
+  }
+]);
